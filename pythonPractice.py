ad89744982920e8a37cce64b3722fc31370b3cbf
priyesh-python <74640825+priyesh-python@users.noreply.github.com>
2020-11-18 11:03:13 +0530

    Add files via upload

---
+++ pythonPractice.py
@@ -0,0 +1,9 @@
+#to add 2 numbers
+
+
+num1 = input("First Number : ")
+num2 = input("\nSecond Number : ")
+
+sum = float(num1) + float(num2)
+
+print("the sum of {0} and {1} is {2}".format(num1,num2,sum))
